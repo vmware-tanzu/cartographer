94a7f0743f4a88430c70edb554ba5ac5ec65a0f0
Ciro S. Costa <ciroscosta@vmware.com>
2021-09-01 14:14:14 -0400

    hack: include copyright
    Signed-off-by: Ciro S. Costa <ciroscosta@vmware.com>

--- hack/ci/e2e.sh
+++ hack/ci/e2e.sh
@@ -1,4 +1,18 @@
 #!/usr/bin/env bash
+# Copyright 2021 VMware
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 
 set -o errexit
 set -o nounset
@@ -18,11 +32,11 @@ readonly COMMAND=${1:-run}
 main() {
         case $COMMAND in
         run)
-                # display_vars
-                # start_registry
-                # start_local_cluster
+                display_vars
+                start_registry
+                start_local_cluster
 
-                # generate_cartographer_release
+                generate_cartographer_release
                 install_dependencies
 
                 setup_example
