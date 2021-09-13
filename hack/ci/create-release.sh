942cdafc6afd9aeba45e16479287b58c3785d629
Sam Coward <scoward@vmware.com>
2021-09-13 16:55:15 -0400

    Use carvel setup action; clarify intent.
    [#7]
    
    Co-authored-by: Todd Ritchie <tritchie@vmware.com>

--- hack/ci/create-release.sh
+++ hack/ci/create-release.sh
@@ -13,32 +13,28 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -eo pipefail
+set -euo pipefail
+
+SCRIPT_PATH=$(dirname $0)
 
 export DEBIAN_FRONTEND=noninteractive
-apt-get -y update && apt-get install -y wget curl perl ca-certificates gnupg lsb-release && update-ca-certificates && rm -rf /var/lib/apt/lists/*
-
-bash -c "set -eo pipefail; curl -L https://carvel.dev/install.sh | bash"
-
-ytt version && kapp version && kbld version && kwt version && imgpkg version && vendir version
+apt-get -y update
+apt-get install -y curl
 
 curl -L https://github.com/google/ko/releases/download/v0.8.3/ko_0.8.3_Linux_arm64.tar.gz | tar xzf - ko
 mv ko /usr/local/bin/ko
 chmod +x /usr/local/bin/ko
 
-cp hack/ci/docker-login.sh /usr/local/bin/docker-login.sh
-chmod +x /usr/local/bin/docker-login.sh
-
 mkdir -p ~/.docker
-docker-login.sh ~/.docker/config.json
-mkdir -p /usr/local/go/src/github.com/vmware-tanzu
-ln -s $(pwd) /usr/local/go/src/github.com/vmware-tanzu/cartographer
+"${SCRIPT_PATH}/docker-login.sh" ~/.docker/config.json
 
 ytt --ignore-unknown-comments -f ./config | ko resolve -f- > release.yaml
 
 echo "# Change Set" > CHANGELOG.md
-git tag --sort=-v:refname -l "v*" | grep -C1 "${GITHUB_REF##*/}" | tail -2 | xargs printf '%s..%s' | xargs -I{} git -c log.showSignature=false log --pretty=oneline --abbrev-commit --no-decorate --no-color {} >> CHANGELOG.md
+CURRENT_VERSION="${GITHUB_REF##*/}"
+PREVIOUS_VERSION="$(git tag --sort=-v:refname -l 'v*' | grep -A1 "${CURRENT_VERSION}" | tail -n 1)"
+git -c log.showSignature=false log --pretty=oneline --abbrev-commit --no-decorate --no-color "${PREVIOUS_VERSION}..${CURRENT_VERSION}" >> CHANGELOG.md
 
 echo "## Checksums" >> CHANGELOG.md
 shasum=$(sha256sum release.yaml)
-echo "$shasum" >> CHANGELOG.md
+echo '```'"${shasum}"'```' >> CHANGELOG.md
