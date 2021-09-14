61089ca2b517549384003ce7e2f4d33100dd6af1
Todd Ritchie <tritchie+github@pivotal.io>
2021-09-14 10:20:16 -0400

    Fix github action
    Running the release locally in `act` worked, but the script failed when
    running in the real life runner.
    
    [#7]
    Co-authored-by: Waciuma Wanjohi <lwanjohi@vmware.com>
    Co-authored-by: Todd Ritchie <tritchie@vmware.com>

--- hack/ci/create-release.sh
+++ hack/ci/create-release.sh
@@ -18,11 +18,11 @@ set -euo pipefail
 SCRIPT_PATH=$(dirname $0)
 
 export DEBIAN_FRONTEND=noninteractive
-apt-get -y update
-apt-get install -y curl
+sudo apt-get -y update
+sudo apt-get install -y curl
 
-curl -L https://github.com/google/ko/releases/download/v0.8.3/ko_0.8.3_Linux_arm64.tar.gz | tar xzf - ko
-mv ko /usr/local/bin/ko
+curl -L https://github.com/google/ko/releases/download/v0.8.3/ko_0.8.3_Linux_x86_64.tar.gz | tar xzf - ko
+sudo mv ko /usr/local/bin/ko
 chmod +x /usr/local/bin/ko
 
 mkdir -p ~/.docker
