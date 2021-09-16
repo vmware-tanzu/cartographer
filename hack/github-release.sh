58d17aa779928ccf9700f6b9a40d9adb0ac8e767
Ciro S. Costa <ciroscosta@vmware.com>
2021-09-16 14:22:26 +0000

    hack: add github-release script
    this script allows us to publish a github release, be on CI or not. note
    that this does not generate any of the files related to the release - it
    assumes that the files are already there, and so it just makes use of
    github's cli[1] to submitting the release through its api. see
    `hack/release.sh` and `hack/release-notes.sh` for release assets
    generaiton.
    
    example: use the current tag as the release version (default), the
    `release/` directory as the place where `yaml` files to be released can
    be found (default), and `release/CHANGELOG.md` as the release notes
    body (default):
    
    	./hack/github-release.sh
    
    see `readonly` vars in the script for customization.
    
    [1]: https://github.com/cli/cli
    
    Signed-off-by: Ciro S. Costa <ciroscosta@vmware.com>

---
+++ hack/github-release.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+# Copyright 2021 VMware
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+readonly ROOT=$(cd $(dirname $0)/.. && pwd)
+readonly RELEASE_NOTES_FILE=${RELEASE_NOTES:-$ROOT/release/CHANGELOG.md}
+readonly ASSETS_DIR=${ASSETS_DIR:-$ROOT/release}
+
+main() {
+        readonly RELEASE_VERSION="${RELEASE_VERSION:-$(git_current_version)}"
+
+        cd $ROOT
+        submit_release_to_github $RELEASE_VERSION
+}
+
+submit_release_to_github() {
+        local version=$1
+
+        gh release create $version \
+                -F $RELEASE_NOTES_FILE \
+                $(find $ASSETS_DIR -name "*.yaml" -type f)
+}
+
+git_current_version() {
+        git tag --sort=-v:refname -l 'v*' |
+                head -n1
+}
+
+main "$@"
