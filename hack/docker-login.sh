219cf590246e8c92d589380c7b1454c55bfece53
Ciro S. Costa <ciroscosta@vmware.com>
2021-09-16 13:52:57 +0000

    hack: re-introduce docker-login
    the previous docker-login was tailored aimed at a CI environment where
    it'd be OK to always override the docker config file at
    `~/.docker/config.json`.
    
    in this new approach, rather than doing an override of the entire file,
    we leverage `jq`'s ability of patching an existing object to just add an
    extra entry to `$DOCKER_CONFIG` (if it already exists) so that we only
    place a new logging in an additive manner.
    
    this script is very useful for a local installation too as with an
    insecure registry, `docker login` would not proceed without further
    tweaks to `/etc/docker/daemon.json`.
    
    example: dockerhub `foo` user
    
    	DOCKER_USERNAME=foo DOCKER_PASSWORD=bar \
    		./hack/docker-login.sh
    
    example: local registry `admin` user
    
    	DOCKER_USERNAME=admin \
    	DOCKER_PASSWORD=admin \
    	DOCKER_REGISTRY=127.0.0.1:5000 \
    		./hack/docker-login.sh
    
    example: custom docker config json, dockerhub login
    
    	DOCKER_CONFIG=`mktemp -d` \
    	DOCKER_USERNAME=foo \
    	DOCKER_PASSWORD=bar \
    		./hack/docker-login.sh
    
    Signed-off-by: Ciro S. Costa <ciroscosta@vmware.com>

---
+++ hack/docker-login.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+# Copyright 2021 VMware
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+readonly DOCKER_REGISTRY=${DOCKER_REGISTRY:-"https://index.docker.io/v1/"}
+readonly DOCKER_CONFIG=${DOCKER_CONFIG:-"$(realpath ~/.docker/config.json)"}
+readonly DOCKER_USERNAME=$DOCKER_USERNAME
+readonly DOCKER_PASSWORD=$DOCKER_PASSWORD
+
+main() {
+        local auth=$(basic_auth $DOCKER_USERNAME $DOCKER_PASSWORD)
+
+        update_or_create_docker_config $DOCKER_REGISTRY $auth $DOCKER_CONFIG
+}
+
+update_or_create_docker_config() {
+        local server=$1
+        local token=$2
+        local config=$3
+
+        readonly qry='.auths["%s"] = {"auth": "%s"}'
+
+	test -s $config || echo '{}' > $config
+        cat $config | jq "$(printf "$qry" $server $token)" >$config
+}
+
+basic_auth() {
+        local username=$1
+        local password=$2
+
+        printf '%s:%s' $username $password | base64
+}
+
+main "$@"
