00e7e2f648871074f3e5ed9dcbbffe11ba70c629
Ciro S. Costa <ciroscosta@vmware.com>
2021-09-16 14:25:41 +0000

    hack: re-introduce cluster setup
    this commit brings back the functionality that we had for the previous
    `hack/e2e.sh` script, but:
    
    - broken down into separate commands
    - by default, making use of an authenticated registry rather than not
    - removing all resource reservations from our dependencies
    - leveraging secretgen[1] for passing registry credential secrets to all
      namespaces
    - using `kapp-ctrl/Package*` as the way of installing cartographer
    
    with this setup we end up then replicating the setups of the real-world
    much more closely, letting us be aware of the tweaks necessary for
    working with private registries.
    
    by having the funcionality of e2e.sh broken down into different
    commands, it also becomes easier to bring a cluster locally and iterate
    on an example, of install _just_ a subset of the dependencies you want.
    
    for instance:
    
    	1. bring up a local cluster with a registry and base
    	dependencies we'd expect to be there
    	(kapp-ctrl,cert-manager,secretgen):
    
    		./hack/setup.sh cluster
    
    	2. install cartographer using local code, pushing that to the
    	local registry
    
    		./hack/setup.sh cartographer
    
    	3. setup the extra dependencies our examples use (like kpack,
    	knative, etc)
    
    		./hack/setup.sh example-dependencies
    
    	4. run the example end to end
    
    		./hack/setup.sh example
    
    [1]: https://github.com/vmware-tanzu/carvel-secretgen-controller
    
    Signed-off-by: Ciro S. Costa <ciroscosta@vmware.com>

---
+++ hack/ip.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# Copyright 2021 VMware
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+from __future__ import print_function
+import socket
+
+def get_ip_address():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    return s.getsockname()[0]
+
+print(get_ip_address())
