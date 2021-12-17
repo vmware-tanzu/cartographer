f1d23110118990f330f1000e312f9e7bc13158b3
Emily Johnson <emjohnson@vmware.com>
2021-12-17 10:19:55 -0600

    Fix merge
    Co-authored-by: Marty Spiewak <mspiewak@vmware.com>

--- pkg/repository/repository.go
+++ pkg/repository/repository.go
@@ -17,14 +17,16 @@ package repository
 import (
 	"context"
 	"fmt"
-	"k8s.io/apimachinery/pkg/types"
+
 	"github.com/go-logr/logr"
 	corev1 "k8s.io/api/core/v1"
 	kerrors "k8s.io/apimachinery/pkg/api/errors"
-	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+
 	"github.com/vmware-tanzu/cartographer/pkg/apis/v1alpha1"
 	"github.com/vmware-tanzu/cartographer/pkg/logger"
 )
