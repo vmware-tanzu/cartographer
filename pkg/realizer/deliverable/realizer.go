50cd0d80d1ec1d9ee95c1aac800efa959e6d5c7f
Marty Spiewak <mspiewak@vmware.com>
2022-01-27 16:04:45 -0500

    Add for deliverable

--- pkg/realizer/deliverable/realizer.go
+++ pkg/realizer/deliverable/realizer.go
@@ -43,6 +43,7 @@ func (r *realizer) Realize(ctx context.Context, resourceRealizer ResourceRealize
 
 	outs := NewOutputs()
 	var stampedObjects []*unstructured.Unstructured
+	var errorToReturn error
 
 	for i := range delivery.Spec.Resources {
 		resource := delivery.Spec.Resources[i]
@@ -56,11 +57,14 @@ func (r *realizer) Realize(ctx context.Context, resourceRealizer ResourceRealize
 		}
 		if err != nil {
 			log.Error(err, "failed to realize resource")
-			return stampedObjects, err
+
+			if errorToReturn == nil {
+				errorToReturn = err
+			}
 		}
 
 		outs.AddOutput(resource.Name, out)
 	}
 
-	return stampedObjects, nil
+	return stampedObjects, errorToReturn
 }
