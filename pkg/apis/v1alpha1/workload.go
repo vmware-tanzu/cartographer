12246318e42e607492493ffa109169ed742b0e5d
BG <bguttmann@anynines.com>
2022-02-16 09:16:31 +0100

    Add Age information to workload CRD

--- pkg/apis/v1alpha1/workload.go
+++ pkg/apis/v1alpha1/workload.go
@@ -75,6 +75,7 @@ var ValidWorkloadPrefixes = []string{
 // +kubebuilder:printcolumn:name="SupplyChain",type="string",JSONPath=".status.supplyChainRef.name"
 // +kubebuilder:printcolumn:name="Ready",type="string",JSONPath=`.status.conditions[?(@.type=='Ready')].status`
 // +kubebuilder:printcolumn:name="Reason",type="string",JSONPath=`.status.conditions[?(@.type=='Ready')].reason`
+// +kubebuilder:printcolumn:name="Age",type="date",JSONPath=`.metadata.creationTimestamp`
 
 type Workload struct {
 	metav1.TypeMeta   `json:",inline"`
