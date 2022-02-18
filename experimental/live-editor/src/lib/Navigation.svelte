81e60e18a766868a759fbcf04da87c3371113512
Rasheed Abdul-Aziz <squeedee@users.noreply.github.com>
2022-02-18 14:15:19 -0500

    Added help

---
+++ experimental/live-editor/src/lib/Navigation.svelte
@@ -0,0 +1,23 @@
+<script>
+    import {getContext} from "svelte";
+    import Help from "./Help.svelte";
+
+    const { open } = getContext('simple-modal');
+
+    const showHelp = () => {
+        open(Help)
+    }
+
+</script>
+
+
+<nav class="font-sans flex flex-col text-center sm:flex-row sm:text-left sm:justify-between py-2 px-6 bg-sky-700 text-sky-50 shadow sm:items-baseline w-full">
+    <div class="mb-2 sm:mb-0">
+        <h1 class="text-lg uppercase">Cartographer Live Editor</h1>
+    </div>
+    <div>
+        <button class="text-lg no-underline text-grey-darkest hover:text-orange-300 ml-2" on:click={showHelp}>
+            Help
+        </button>
+    </div>
+</nav>
