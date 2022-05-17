6abfa4981f77390e839e48367ca4a081cbdfee4b
Sam Coward <scoward@vmware.com>
2022-05-17 12:50:04 -0400

    unfocus test

--- tests/integration/delivery/deliverable_reconciler_test.go
+++ tests/integration/delivery/deliverable_reconciler_test.go
@@ -37,7 +37,7 @@ import (
 	"github.com/vmware-tanzu/cartographer/tests/resources"
 )
 
-var _ = FDescribe("DeliverableReconciler", func() {
+var _ = Describe("DeliverableReconciler", func() {
 	var templateBytes = func() []byte {
 		configMap := &corev1.ConfigMap{
 			TypeMeta: metav1.TypeMeta{
