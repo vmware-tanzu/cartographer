4f022122fbb3903cd948c72829ec0e2833a132e3
Rasheed Abdul-Aziz <squeedee@users.noreply.github.com>
2022-05-18 12:15:31 -0400

    Singular realizer (#857)
    Workload and Deliverable share common realizer
    
    
    Co-authored-by: Sam Coward <scoward@vmware.com>
    Co-authored-by: Rasheed Abdul-Aziz <squeedee@users.noreply.github.com>

--- pkg/realizer/outputs_test.go
+++ pkg/realizer/outputs_test.go
@@ -12,14 +12,14 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package deliverable_test
+package realizer_test
 
 import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
 	"github.com/vmware-tanzu/cartographer/pkg/apis/v1alpha1"
-	realizer "github.com/vmware-tanzu/cartographer/pkg/realizer/deliverable"
+	"github.com/vmware-tanzu/cartographer/pkg/realizer"
 	"github.com/vmware-tanzu/cartographer/pkg/templates"
 )
 
@@ -40,7 +40,7 @@ var _ = Describe("Outputs", func() {
 
 			Context("And the sources have a match with the outputs", func() {
 				It("Adds sources to inputs", func() {
-					resource := &v1alpha1.DeliveryResource{
+					resource := realizer.OwnerResource{
 						Sources: []v1alpha1.ResourceReference{
 							{
 								Name:     "source-ref",
@@ -58,7 +58,7 @@ var _ = Describe("Outputs", func() {
 
 			Context("And the sources do not have a match with the outputs", func() {
 				It("Does not add sources to inputs", func() {
-					resource := &v1alpha1.DeliveryResource{
+					resource := realizer.OwnerResource{
 						Sources: []v1alpha1.ResourceReference{
 							{
 								Name:     "source-ref",
@@ -70,6 +70,50 @@ var _ = Describe("Outputs", func() {
 					Expect(len(inputs.Sources)).To(Equal(0))
 				})
 			})
+		})
+
+		Context("When resource contains images", func() {
+			var outs realizer.Outputs
+			BeforeEach(func() {
+				outs = realizer.NewOutputs()
+				imageOutput := &templates.Output{
+					Image: "image12345",
+				}
+				outs.AddOutput("image-output", imageOutput)
+			})
+
+			Context("And the images have a match with the outputs", func() {
+				It("Adds images to inputs", func() {
+					resource := realizer.OwnerResource{
+						Images: []v1alpha1.ResourceReference{
+							{
+								Name:     "image-ref",
+								Resource: "image-output",
+							},
+						},
+					}
+					inputs := outs.GenerateInputs(resource)
+					Expect(inputs.Images).To(HaveLen(1))
+					Expect(inputs.Images["image-ref"].Name).To(Equal("image-ref"))
+					Expect(inputs.Images["image-ref"].Image).To(Equal("image12345"))
+				})
+			})
+
+			Context("And the images do not have a match with the outputs", func() {
+				It("Does not add images to inputs", func() {
+					resource := realizer.OwnerResource{
+						Images: []v1alpha1.ResourceReference{
+							{
+								Name:     "image-ref",
+								Resource: "image-output-does-not-exist",
+							},
+						},
+					}
+					inputs := outs.GenerateInputs(resource)
+					Expect(inputs.Sources).To(BeEmpty())
+				})
+			})
+
 		})
 
 		Context("When resource contains configs", func() {
@@ -84,7 +128,7 @@ var _ = Describe("Outputs", func() {
 
 			Context("And the configs have a match with the outputs", func() {
 				It("Adds configs to inputs", func() {
-					resource := &v1alpha1.DeliveryResource{
+					resource := realizer.OwnerResource{
 						Configs: []v1alpha1.ResourceReference{
 							{
 								Name:     "config-ref",
@@ -101,7 +145,7 @@ var _ = Describe("Outputs", func() {
 
 			Context("And the configs do not have a match with the outputs", func() {
 				It("Does not add configs to inputs", func() {
-					resource := &v1alpha1.DeliveryResource{
+					resource := realizer.OwnerResource{
 						Configs: []v1alpha1.ResourceReference{
 							{
 								Name:     "config-ref",
