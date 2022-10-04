7a2d866fb025295710df0682c2cf2e5779a38431
Waciuma Wanjohi <lwanjohi@vmware.com>
2022-10-04 17:12:07 -0400

    Lint

--- pkg/testing/cli-suite-builder.go
+++ pkg/testing/cli-suite-builder.go
@@ -1,15 +1,30 @@
+// Copyright 2021 VMware
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package testing
 
 import (
 	"errors"
 	"fmt"
-	"github.com/vmware-tanzu/cartographer/pkg/apis/v1alpha1"
-	"github.com/vmware-tanzu/cartographer/pkg/templates"
 	"io/fs"
 	"os"
 	"path/filepath"
 
 	log "github.com/sirupsen/logrus"
+
+	"github.com/vmware-tanzu/cartographer/pkg/apis/v1alpha1"
+	"github.com/vmware-tanzu/cartographer/pkg/templates"
 )
 
 type testInfo struct {
