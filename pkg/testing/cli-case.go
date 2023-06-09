355c7234aa8d060ca065fbc9f3c8ba96eb91bddb
Waciuma Wanjohi <lwanjohi@vmware.com>
2023-06-09 07:58:06 -0400

    populate TestCase SupplyChain function

--- pkg/testing/cli-case.go
+++ pkg/testing/cli-case.go
@@ -6,6 +6,7 @@ import (
 	"io/fs"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/vmware-tanzu/cartographer/pkg/apis/v1alpha1"
 	"github.com/vmware-tanzu/cartographer/pkg/realizer"
@@ -45,17 +46,18 @@ type testInfoMockSC struct {
 }
 
 type testInfoSupplyChain struct {
-	Path               *string           `yaml:"path"`
+	Paths              []string          `yaml:"paths"`
 	YttPath            *string           `yaml:"yttPath"`
 	TargetResourceName *string           `yaml:"targetResourceName"`
 	PreviousOutputs    *realizer.Outputs `yaml:"previousOutputs"`
 }
 
 const (
-	templateDefaultFilename  = "template.yaml"
-	workloadDefaultFilename  = "workload.yaml"
-	expectedDefaultFilename  = "expected.yaml"
-	yttValuesDefaultFilename = "ytt-values.yaml"
+	templateDefaultFilename             = "template.yaml"
+	workloadDefaultFilename             = "workload.yaml"
+	expectedDefaultFilename             = "expected.yaml"
+	templateYttValuesDefaultFilename    = "template-ytt-values.yaml"
+	supplyChainYttValuesDefaultFilename = "supply-chain-ytt-values.yaml"
 )
 
 func populateTestCase(testCase *TemplateTestCase, directory string) (*TemplateTestCase, error) {
@@ -76,7 +78,7 @@ func populateTestCase(testCase *TemplateTestCase, directory string) (*TemplateTe
 
 	newExpectedFilePath, err := getLocallySpecifiedPath(directory, expectedDefaultFilename, info.Expected)
 	if err != nil {
-		return nil, fmt.Errorf("replace expected file in directory %s: %w", directory, err)
+		return nil, fmt.Errorf("get expected file specified in directory %s: %w", directory, err)
 	}
 	if newExpectedFilePath != "" {
 		testCase.Expect = &ExpectedFile{Path: newExpectedFilePath}
@@ -119,7 +121,7 @@ func populateTestCase(testCase *TemplateTestCase, directory string) (*TemplateTe
 func populateTestCaseWorkload(testCase *TemplateTestCase, directory string, info *testInfo) (*TemplateTestCase, error) {
 	newWorkloadValue, err := getLocallySpecifiedPath(directory, workloadDefaultFilename, info.Given.Workload)
 	if err != nil {
-		return nil, fmt.Errorf("replace workload file in directory %s: %w", directory, err)
+		return nil, fmt.Errorf("get workload file specified in directory %s: %w", directory, err)
 	}
 	if newWorkloadValue != "" {
 		testCase.Given.Workload = &WorkloadFile{Path: newWorkloadValue}
@@ -128,26 +130,24 @@ func populateTestCaseWorkload(testCase *TemplateTestCase, directory string, info
 }
 
 func populateTestCaseTemplate(testCase *TemplateTestCase, directory string, info *testInfo) (*TemplateTestCase, error) {
-	newTemplateFilepath, err := getLocallySpecifiedPath(directory, templateDefaultFilename, info.Given.Template.Path)
-	if err != nil {
-		return nil, fmt.Errorf("replace template file in directory %s: %w", directory, err)
-	}
-
-	yttFile, err := getLocallySpecifiedPath(directory, yttValuesDefaultFilename, info.Given.Template.YttPath)
-	if err != nil {
-		return nil, fmt.Errorf("replace workload file in directory %s: %w", directory, err)
-	}
-
 	newTemplateFile := TemplateFile{}
 
 	if previousTemplateFile, prevTemplateFileExisted := testCase.Given.Template.(*TemplateFile); prevTemplateFileExisted {
 		newTemplateFile = *previousTemplateFile
 	}
 
+	newTemplateFilepath, err := getLocallySpecifiedPath(directory, templateDefaultFilename, info.Given.Template.Path)
+	if err != nil {
+		return nil, fmt.Errorf("get template file specified in directory %s: %w", directory, err)
+	}
 	if newTemplateFilepath != "" {
 		newTemplateFile.Path = newTemplateFilepath
 	}
 
+	yttFile, err := getLocallySpecifiedPath(directory, templateYttValuesDefaultFilename, info.Given.Template.YttPath)
+	if err != nil {
+		return nil, fmt.Errorf("get template ytt file specified in directory %s: %w", directory, err)
+	}
 	if yttFile != "" {
 		newTemplateFile.YttFiles = []string{yttFile}
 	}
@@ -177,8 +177,45 @@ func populateTestCaseMockSupplyChain(testCase *TemplateTestCase, info *testInfo)
 }
 
 func populateTestCaseSupplyChain(testCase *TemplateTestCase, directory string, info *testInfo) (*TemplateTestCase, bool, error) {
-	panic("not implemented")
-	//return testCase, false, nil
+	var supplyChainSpecified bool
+
+	newSupplyChain := SupplyChainFileSet{}
+
+	if previousSCSet, prevSCSetExisted := testCase.Given.SupplyChain.(*SupplyChainFileSet); prevSCSetExisted {
+		newSupplyChain = *previousSCSet
+	}
+
+	if info.Given.SupplyChain.TargetResourceName != nil {
+		newSupplyChain.TargetResourceName = *info.Given.SupplyChain.TargetResourceName
+	}
+
+	if info.Given.SupplyChain.PreviousOutputs != nil {
+		newSupplyChain.PreviousOutputs = info.Given.SupplyChain.PreviousOutputs
+	}
+
+	yttFile, err := getLocallySpecifiedPath(directory, supplyChainYttValuesDefaultFilename, info.Given.SupplyChain.YttPath)
+	if err != nil {
+		return nil, false, fmt.Errorf("get supply chain ytt file specified in directory %s: %w", directory, err)
+	}
+	if yttFile != "" {
+		newSupplyChain.YttFiles = []string{yttFile}
+	}
+
+	if len(info.Given.SupplyChain.Paths) > 0 {
+		newSupplyChain.Paths = info.Given.SupplyChain.Paths
+		supplyChainSpecified = true
+	} else {
+		filesPrefixedSupplyChainInDir, err := getFilesPrefixedSupplyChainInDir(directory)
+		if err != nil {
+			return nil, false, fmt.Errorf("get files prefixed with supply-chain in dir: %w", err)
+		}
+		if len(filesPrefixedSupplyChainInDir) > 0 {
+			newSupplyChain.Paths = filesPrefixedSupplyChainInDir
+			supplyChainSpecified = true
+		}
+	}
+
+	return testCase, supplyChainSpecified, nil
 }
 
 // getLocallySpecifiedPath returns the path specified in info.yaml, or if none specified there, the default
@@ -195,4 +232,21 @@ func getLocallySpecifiedPath(directory, filename string, priorityPath *string) (
 		return "", fmt.Errorf("failed while getting file info on %s: %w", candidatePath, err)
 	}
 	return candidatePath, nil
+}
+
+func getFilesPrefixedSupplyChainInDir(directory string) ([]string, error) {
+	files, err := os.ReadDir(directory)
+	if err != nil {
+		return nil, fmt.Errorf("os read directory: %w", err)
+	}
+
+	var supplyChainYamlFilesFound []string
+
+	for _, file := range files {
+		if strings.HasPrefix(file.Name(), "supply-chain") && strings.HasSuffix(file.Name(), ".yaml") {
+			supplyChainYamlFilesFound = append(supplyChainYamlFilesFound, filepath.Join(directory, file.Name()))
+		}
+	}
+
+	return supplyChainYamlFilesFound, nil
 }
